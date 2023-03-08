872d7564bcdbd9634e989c56b6133ec53c350109
dobromirts <dobromir.ts@gmail.com>
2023-03-08 11:33:34 +0200

    test gesture override

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+@Injectable()
+export class CarouselHammerConfig extends HammerGestureConfig {
+  public hammer = new Hammer(
+    document.getElementById('gesture-element') as HTMLElement | SVGElement,
+    { inputClass: Hammer.TouchMouseInput }
+  );
+
+  public override overrides = {
+    swipe: { enable: true, direction: Hammer.DIRECTION_HORIZONTAL },
+    pan: { enable: true, direction: Hammer.DIRECTION_HORIZONTAL },
+    pinch: { enable: false },
+    rotate: { enable: false },
+  };
+}
 
 @Component({
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: CarouselHammerConfig
+    }
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
@@ -13,5 +35,5 @@ export class AppComponent {
     {
       src: 'https://www.infragistics.com/angular-demos-lob/assets/images/svg/carousel/Route.svg'
     }
-];
+  ];
 }
